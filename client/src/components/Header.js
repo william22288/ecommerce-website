1e00327198531c9b9fe17005a88c149cd120719a
ttan0098 <ttan0098@student.monash.edu>
2024-12-23 17:52:08 +1100

    edited codes to toggle visibility of items in cart

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,46 @@
-import React, {useContext} from "react";
+import React, { useContext, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { itemContext } from '../context/ItemContext';
 
 const Navbar = () => {
-    const { itemsInCart, totalPrice } = useContext(itemContext);
+  const { cart } = useContext(itemContext);
+  const [isCartVisible, setIsCartVisible] = useState(false);
 
-    return (
-        <nav className='navbar'>
-            <div className='navbar-brand'>
-                <h1 className='ecommerce'>E-Commerce</h1>
-            </div>
-            <div className='navbar-items'>
-                <h3 style={{color: 'white'}}>
-                    total price: ${totalPrice}
-                </h3>
-                <div className='cart-num'>
-                    <FontAwesomeIcon icon={faCartShopping} size="2x" />
-                    <span>{itemsInCart}</span>
+  const toggleCartVisibility = () => {
+    setIsCartVisible(!isCartVisible);
+  };
+
+  return (
+    <nav className='navbar'>
+      <div className='navbar-brand'>
+        <h1 className='ecommerce'>E-Commerce</h1>
+      </div>
+      <div className='navbar-items'>
+        <div className='cart-num' onClick={toggleCartVisibility}>
+          <FontAwesomeIcon icon={faCartShopping} size="2x" />
+          <span>{cart.length}</span>
+        </div>
+        {isCartVisible && (
+          <div className='cart-dropdown'>
+            {cart.length > 0 ? (
+              cart.map((item) => (
+                <div key={item._id} className='cart-item'>
+                  <img src={item.image} alt={item.name} />
+                  <div>
+                    <h4>{item.name}</h4>
+                    <p>${item.price}</p>
+                  </div>
                 </div>
-            </div>
-        </nav>
-    )
-}
+              ))
+            ) : (
+              <p>Your cart is empty</p>
+            )}
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
